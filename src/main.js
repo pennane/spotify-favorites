f2c8a171dcc9528c6003010d58ea927ff68388dc
Arttu Pennanen <arttu@pennanen.org>
2020-07-07 00:47:04 +0300

    Added analytics

--- src/main.js
+++ src/main.js
@@ -3,7 +3,18 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import VueAnalytics from 'vue-analytics'
+
 Vue.config.productionTip = false;
+
+Vue.use(VueAnalytics, {
+  id: 'UA-126133790-2',
+  debug: {
+    sendHitTask: process.env.NODE_ENV === 'production'
+  },
+  router
+})
+
 
 new Vue({
   router,
